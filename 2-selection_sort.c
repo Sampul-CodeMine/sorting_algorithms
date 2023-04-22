f0246dfe0eb462f5e811472e5b1e57c10a263347
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-22 17:26:53 +0100

    Solution to Task 2 - Selection Sort - Sorting Algorithm and Big O Notation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-void s_swap(int *, int *);
-
 /**
  * selection_sort - algorithm to sort an array of integers.
  * This is a very slow algorithm
@@ -9,39 +7,36 @@ void s_swap(int *, int *);
  * @size: size of array
  * Return: Nothing
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t itr = 0;
 	size_t steps = 0;
-	size_t init_low;
+	int init_min_val, position;
+	bool swapped = false;
 
-	if (!array || size < 2)
+	if (!array || array == NULL || size < 2)
 		return;
 
-	for (steps = 0; steps < (size - 1); steps++)
+	for (steps = 0; steps < size; steps++)
 	{
-		init_low = steps;
+		position = steps;
+		init_min_val = array[steps];
 		for (itr = (steps + 1); itr < size; itr++)
 		{
-			if (array[itr] < array[init_low])
-				init_low = itr;
+			if (array[itr] < init_min_val)
+			{
+				init_min_val = array[itr];
+				position = itr;
+				swapped = true;
+			}
 		}
-		s_swap(&array[init_low], &array[steps]);
-		print_array(array, size);
-	}
-}
-
-/**
- * s_swap - swap array elements
- * @val_a_ptr: ptr to array element
- * @val_b_ptr: prt to array element
- */
-
-void s_swap(int *val_a_ptr, int *val_b_ptr)
-{
-	int temp;
-
-	temp = *val_a_ptr;
-	*val_a_ptr = *val_b_ptr;
-	*val_b_ptr = temp;
+		array[position] = array[steps];
+		array[steps] = init_min_val;
+		if (swapped)
+		{
+			print_array(array, size);
+			swapped = false;
+		}
+	}	
 }
